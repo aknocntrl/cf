53e51f5103b8accb4a1cb1dca6d23a3b3034599f
aknocntrl <aksharamishra02.0@gmail.com>
2026-07-05 12:13:27 +0530

    Add Short Substrings [A] from Codeforces

---
+++ 1367/A - Short Substrings/solution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string b;
+        cin>>b;
+        string a="";
+        a+=b[0];
+        for(int i=1;i<b.length()-1;i+=2){
+            a+=b[i];}
+        a+=b[b.length()-1];
+        cout<<a<<endl;}
+    return 0;
+}
