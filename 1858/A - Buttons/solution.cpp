319797c0ea41e4ae63aba26acecdfbb514685d25
aknocntrl <aksharamishra02.0@gmail.com>
2026-07-05 12:12:46 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        int anna=a+(c+1)/2;
+        int katie=b+c/2;
+        if(anna>katie)
+            cout<<"First
+";
+        else
+        cout<<"Second
+";}
+    return 0;
+}
