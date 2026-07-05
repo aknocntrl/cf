330a0560a69e15ee81846f1976f5ed792de767c0
aknocntrl <aksharamishra02.0@gmail.com>
2026-07-05 12:18:45 +0530

    Add Vasya the Hipster [A] from Codeforces

---
+++ 581/A - Vasya the Hipster/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int x=min(a,b);
+    a-=x;
+    b-=x;
+    int y=max(a/2,b/2);
+    cout<<x<<" "<<y;
+    return 0;
+}
