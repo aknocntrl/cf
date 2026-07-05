7aa3c854cf89f646cc1cb203d469b96caeac3bcf
aknocntrl <aksharamishra02.0@gmail.com>
2026-07-05 12:10:15 +0530

    Add Short Sort [A] from Codeforces

---
+++ 1873/A - Short Sort/solution.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s=="abc" || s=="acb" || s=="bac" || s=="cba"){
+            cout<<"YES"<<endl;}
+        else
+        cout<<"NO"<<endl;}
+    return 0;
+}
