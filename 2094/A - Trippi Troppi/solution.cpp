83031de07f0fa4efb5cd0ee4e10792422ee343f0
aknocntrl <aksharamishra02.0@gmail.com>
2026-07-05 12:12:00 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string x,y,z;
+        cin>>x>>y>>z;
+        cout<<x[0]<<y[0]<<z[0]<<endl;}
+    return 0;
+}
