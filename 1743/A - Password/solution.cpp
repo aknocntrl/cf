badb58179e09681f27bca5c070993d09206ccca2
aknocntrl <aksharamishra02.0@gmail.com>
2026-07-05 12:16:49 +0530

    Add Password [A] from Codeforces

---
+++ 1743/A - Password/solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int notavl=n;
+        while(n--){
+            int a;
+            cin>>a;}
+        int avl=10-notavl;
+        int way=(avl*(avl-1))/2;
+        int ans=way*6;
+        cout<<ans<<endl;}
+    return 0;
+}
