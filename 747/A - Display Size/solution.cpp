62354da1fe8e3084910011b341f53b08fc9450c2
aknocntrl <aksharamishra02.0@gmail.com>
2026-07-05 12:18:10 +0530

    Add Display Size [A] from Codeforces

---
+++ 747/A - Display Size/solution.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=sqrt(n);i>=1;i--){
+        if(n%i==0){
+            cout<<i<<' '<<n/i<<endl;
+            break;}}
+    return 0;
+}
