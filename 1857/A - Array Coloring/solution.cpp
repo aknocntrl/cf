4fb9e5b3505a6de8c70c16fa22c172d14b5b297e
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-08 12:29:04 +0530

    Add Array Coloring [A] from Codeforces

---
+++ 1857/A - Array Coloring/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            if(a%2!=0){
+                ans++;}}
+        if(ans%2==0){
+            cout<<"YES"<<endl;}
+        else
+        cout<<"NO"<<endl;}
+    return 0;
+}
