49ab9678a599cfd886fbc0dae099f6fc2e05e0b8
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-09 05:35:31 +0530

    Add Word on the Paper [C] from Codeforces

---
+++ 1850/C - Word on the Paper/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                char a;
+                cin>>a;
+                if(a!='.'){
+                    cout<<a;}}}
+        cout<<endl;}
+    return 0;
+}
