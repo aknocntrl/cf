6751b61022007d2c12a15ed6485292b647891a3a
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-10 10:06:33 +0530

    Add Skibidus and Amog'u [A] from Codeforces

---
+++ 2065/A - Skibidus and Amog'u/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string w;
+        cin>>w;
+        w.pop_back();
+        w.pop_back();
+        cout<<w+'i'<<endl;}
+    return 0;
+}
