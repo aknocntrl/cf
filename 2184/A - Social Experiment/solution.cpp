764904b5dee38bd8b18a7da9aa83365b27d8350b
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-24 20:26:06 +0530

    Add Social Experiment [A] from Codeforces

---
+++ 2184/A - Social Experiment/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n==2){
+            cout<<2<<endl;}
+        if(n==3){
+            cout<<3<<endl;}
+        if(n>=4){
+            cout<<n%2<<endl;}}
+    return 0;
+}
