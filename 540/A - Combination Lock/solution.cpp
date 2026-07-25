383296664111b84949af4e73ca6a0bf6a4b0fa33
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-25 19:02:46 +0530

    Add Combination Lock [A] from Codeforces

---
+++ 540/A - Combination Lock/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string a,b;
+    cin>>a>>b;
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans+=min((abs(a[i]-b[i])),10-abs(a[i]-b[i]));}
+    cout<<ans;
+    return 0;
+}
