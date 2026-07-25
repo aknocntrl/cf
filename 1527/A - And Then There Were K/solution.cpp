243bdff315509044375b4023fa35c52477a194e4
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-25 12:51:54 +0530

    Add And Then There Were K [A] from Codeforces

---
+++ 1527/A - And Then There Were K/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans=0;
+        for(int i=0;i<30;i++){
+            if(((n>>i)&1)==1){
+                ans=i;}}
+        cout<<(1<<ans)-1<<endl;}
+    return 0;
+}
