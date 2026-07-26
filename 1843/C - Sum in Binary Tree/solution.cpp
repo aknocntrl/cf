17d2b5bf62d49e770cd0c79743af1d85bd44e782
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-26 14:18:44 +0530

    Add Sum in Binary Tree [C] from Codeforces

---
+++ 1843/C - Sum in Binary Tree/solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,s=0;
+        cin>>n;
+        while(n>=1){
+            s+=n;
+            n/=2;}
+        cout<<s<<endl;}
+    return 0;
+}
