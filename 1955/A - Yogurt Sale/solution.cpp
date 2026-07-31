25309924d551bc87243047a0cd177ba6d131e09d
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-31 15:34:04 +0530

    Add Yogurt Sale [A] from Codeforces

---
+++ 1955/A - Yogurt Sale/solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,a,b;
+        cin>>n>>a>>b;
+        long long ans=n*a;
+        if(b<2*a){
+            ans=(n/2)*b+(n%2)*a;}
+        cout<<ans<<endl;}
+    return 0;
+}
