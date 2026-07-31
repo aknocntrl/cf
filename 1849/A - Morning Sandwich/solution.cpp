2c6d600c80b7d38d8d2cdcb83cfe920d0702d0cc
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-31 21:15:39 +0530

    Add Morning Sandwich [A] from Codeforces

---
+++ 1849/A - Morning Sandwich/solution.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int b,c,h;
+        cin>>b>>c>>h;
+        cout<<(2*min(b-1,c+h)+1)<<endl;}
+    return 0;
+}
