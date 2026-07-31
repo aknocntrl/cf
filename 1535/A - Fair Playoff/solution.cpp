bbcf614d73583cde08e2a22f1bd19ac690516117
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-31 15:55:04 +0530

    Add Fair Playoff [A] from Codeforces

---
+++ 1535/A - Fair Playoff/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector<int> s(4);
+        for(int& x:s){
+            cin>>x;}
+        if(min(s[0],s[1])>max(s[2],s[3]) || max(s[0],s[1])<min(s[2],s[3])){
+            cout<<"NO"<<endl;}
+        else{
+            cout<<"YES"<<endl;}}
+    return 0;
+}
