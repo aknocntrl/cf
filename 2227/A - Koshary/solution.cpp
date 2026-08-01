8fd853a340f7e0c722cb0a8dca55ba7d0e5fa7d6
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-08-01 13:36:29 +0530

    Add Koshary [A] from Codeforces

---
+++ 2227/A - Koshary/solution.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<(x%2==0 || y%2==0?"YES":"NO")<<endl;}
+    return 0;
+}
