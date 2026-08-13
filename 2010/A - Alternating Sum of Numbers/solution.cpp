5557cb8f48ffe40c02c64187c557f780802b4dc7
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-08-13 23:41:00 +0530

    Add Alternating Sum of Numbers [A] from Codeforces

---
+++ 2010/A - Alternating Sum of Numbers/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            if(i%2==0){
+                ans+=x;}
+            else{
+                ans-=x;}}
+        cout<<ans<<endl;}
+    return 0;
+}
