c1d5222898f6a38faf587b0acb5c376aa69f4afb
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-08-26 12:09:45 +0530

    Add K-th Not Divisible by n [C] from Codeforces

---
+++ 1352/C - K-th Not Divisible by n/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int ans=k+(k-1)/(n-1);
+        cout<<ans<<endl;}
+    return 0;
+}
